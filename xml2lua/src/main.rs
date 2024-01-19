e20e5374df55360f89a6b497f225c129ca12c8f6
luckinbyte <531671505@qq.com>
2024-01-19 18:01:25 +0800

    a

--- xml2lua/src/main.rs
+++ xml2lua/src/main.rs
@@ -1,3 +1,12 @@
+extern crate calamine;
+
+use calamine::{open_workbook, Reader, Xlsx};
+
 fn main() {
-    println!("Hello, world!");
+    let path = format!("./ee.xlsx");
+    let mut excel:Xlsx<_> = open_workbook(&path).unwrap();
+    let sheets:Vec<String> = excel.sheet_names().to_owned();
+    for s in sheets{
+        let mut cells_reader = excel.worksheet_cells_reader(&s).unwrap();
+    }
 }
